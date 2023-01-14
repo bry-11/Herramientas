822e3884432f2f5ed04eb978378b79d29c1f182f
bry-11 <bryan.r11234@gmail.com>
2023-01-13 21:02:54 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,47 @@
+[
+    {
+        "alumno": "Bryan",
+        "nota": 4.09,
+        "ranking": 100
+    },
+    {
+        "alumno": "Paul",
+        "nota": 6.09,
+        "ranking": 200
+    },
+    {
+        "alumno": "Jose",
+        "nota": 2.09,
+        "ranking": 150
+    },
+    {
+        "alumno": "Manuel",
+        "nota": 8.09,
+        "ranking": 50
+    },
+    {
+        "alumno": "Raul",
+        "nota": 7.09,
+        "ranking": 300
+    },
+    {
+        "alumno": "Pepe",
+        "nota": 1.09,
+        "ranking": 350
+    },
+    {
+        "alumno": "Ariel",
+        "nota": 5.09,
+        "ranking": 220
+    },
+    {
+        "alumno": "Daniel",
+        "nota": 3.09,
+        "ranking": 10
+    },
+    {
+        "alumno": "Pato",
+        "nota": 9.09,
+        "ranking": 320
+    },
+]
